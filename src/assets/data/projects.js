3ba0bddb35a02d8cfd83b06afab4219071e67ad7
Kevin Hernandez <khernandez0810@gmail.com>
2025-12-17 07:20:24 -0700

    clean up projects

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -24,15 +24,6 @@ const projects = [
 
     },
     {
-        id:2,
-        name: 'Note Taker Application',
-        image: noteTaker,
-        alt: 'pic of note taker app',
-        repoLink: 'https://github.com/khernandez0810/Note-Taker-application',
-        deployLink: 'https://github.com/khernandez0810/Note-Taker-application',
-        description: 'An application allowing you to add and delete your notes to keep track of your day'
-    },
-    {
         id:3,
         name: 'Daily Planner',
         image: planner,
@@ -50,15 +41,7 @@ const projects = [
         deployLink: 'https://polar-ridge-60825.herokuapp.com/',
         description: 'A blog page allowing you to sign in and interact with others on your blog thoughts'
     },
-    {
-        id:5,
-        name: 'Sport Trivia Quiz application',
-        image: trivia,
-        alt: 'pic of quiz application',
-        repoLink: 'https://github.com/khernandez0810/TriviaTime',
-        deployLink: 'https://khernandez0810.github.io/TriviaTime/',
-        description: 'A trivia quiz application allowing you to test your knowledge of current and past sports'
-    },
+
     {   
         id: 6,
         name: 'Chop Shop TKO',
